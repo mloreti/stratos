1375e869c222572f9820bb7cf6d1e272ca5b9d6b
Mickey Loreti <mloreti10@gmail.com>
2017-01-06 17:26:01 -0800

    Add input fields for events

--- src/components/events/events.js
+++ src/components/events/events.js
@@ -4,6 +4,7 @@ class Events extends React.Component {
 
   constructor(props) {
     super(props);
+    this.state = {'title': '', duration: 0, time: null}
     this.handleClick = this.handleClick.bind(this);
   }
 
@@ -14,9 +15,43 @@ class Events extends React.Component {
     }
     this.props.addEvent(event);
   }
+
+  update(field) {
+    return e => this.setState({
+      [field]: e.currentTarget.value
+    })
+  }
+
+  times() {
+    let times = this.getTimes("AM").concat(this.getTimes("PM"));
+    return times
+  }
+
+  getTimes(time) {
+    let times = [];
+    for(let i = 1; i < 13; i++){
+      if (i < 10) {
+        times.push(`0${i}:00 ${time}`)
+      } else {
+        times.push(`${i}:00 ${time}`)
+      }
+    }
+    return times;
+  }
+
   render(){
     return(
-      <h1 onClick={this.handleClick}>From Events</h1>
+      <div>
+        <h1>Add Event</h1>
+        <input onChange={this.update("title")} type="text" name="title" placeholder="Title"></input>
+        Duration (in minutes)<input onChange={this.update("duration")} type="number" name="duration" placeholder="Duration"></input>
+      Time <select onChange={this.update("time")}>
+        <option>N/A</option>
+        {this.times().map(time => (
+          <option key={time} value={time}>{time}</option>
+        ))}
+      </select>
+      </div>
     )
   }
 }
