d195a44a98a9bb0e784fa6504c2e4ca98fc164f8
Mickey Loreti <mloreti10@gmail.com>
2017-01-19 17:48:58 -0800

    update user controller

--- api/app/controllers/users_controller.rb
+++ api/app/controllers/users_controller.rb
@@ -1,5 +1,15 @@
 class UsersController < ApplicationController
-  before_action :authenticate_user
+  # before_action :authenticate_user
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      render json: {"status": "200 OK"}
+    else
+      render json: {"error": "Unable to create user"}
+    end
+
+  end
 
   def show
     @user = User.find(params[:id])
@@ -8,5 +18,11 @@ class UsersController < ApplicationController
     else
       render json: {"Error": "404 Not Found"}
     end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :password)
   end
 end
